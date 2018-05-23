a043a29ce409ca5aaf5a30ed6d6112142799dbc4
LDMorCX <577729802@qq.com>
2018-05-23 20:18:40 +0800

    新建beike文件夹

---
+++ 微信小程序/pages/index/index.js
@@ -0,0 +1,90 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    mainInfo:{},
+    houseInfoLists:{},
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.getMainData();
+    this.getHouseLists();
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  getMainData:function(){
+    const that = this;
+    wx.request({
+      url:"http://47.93.220.17/Home/Bk/xinfang",
+      method:"get",
+      dataType:"json",
+      success:function(res){
+        that.setData({
+          mainInfo:res.data,
+        });
+      },
+    })
+  },
+  getHouseLists:function(e){
+    const nowId = e.currentTarget.id;
+    // const nowId = e.currentTarget.id;
+    console.log(nowId);
+    const that = this;
+    wx.request({
+      url: "http://47.93.220.17/Home/Bk/getListsByType",
+      method:"get",
+      dataType:"json",
+      data:{
+        type_id:nowId,
+      },
+      success:function(res){
+        that.setData({
+          houseInfoLists: res.data,
+        });
+        console.log(res)
+      },
+    })
+  },
+})
